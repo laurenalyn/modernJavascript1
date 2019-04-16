f180290e97eae9c6d2dec5fe36777b20df17221e
laurenalyn <laurenalynwilkerson@gmail.com>
2019-04-16 11:16:06 -0400

    example of try - catch - finally with an undefined function

---
+++ testing/testing.js
@@ -0,0 +1,19 @@
+
+try {
+    something(); //undefined function
+} catch(error) {
+    console.log(error); //will let us know if there is an error 
+} finally {
+    console.log('Execute always - no matter what!');
+}
+
+
+function getClients() {
+    console.log('Downloading...');
+
+    setTimeout(function () {
+        console.log('Complete...');
+    }, 3000);
+}
+
+getClients();

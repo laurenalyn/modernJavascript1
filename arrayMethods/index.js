6149374dad485ddb22878495de4520b186699c69
laurenalyn <laurenalynwilkerson@gmail.com>
2019-04-16 11:00:57 -0400

    examples of forEach& map

---
+++ arrayMethods/index.js
@@ -0,0 +1,34 @@
+// for loop
+// const todo = ['homework', 'food', 'project', 'learn js'];
+
+// for(let i = 0; i < todo.length; i++) {
+//     console.log(todo[i]);
+// }
+
+// forEach
+// todo.forEach(function(assignment, index){
+//     console.log(`${index} : ${assignment}`)
+// });
+
+// map
+// const shoppingCart = [
+//     {id:1, product: 'book'},
+//     {id:2, product: 'shirt'},
+//     {id:3, product: 'album'},
+// ];
+// const productName = shoppingCart.map(function(productName){
+//     return productName.product;
+// });
+// console.log(productName);
+
+// iterators in ES6
+let myCar = {
+    model: 'Camaro',
+    engine: 6.0,
+    year: 1969,
+    make: 'Chevy'
+}
+
+for(let key in myCar) {
+    console.log(`${key}: ${myCar[key]}`);
+}
